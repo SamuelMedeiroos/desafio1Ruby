5f2c68f1f955c50a16a8f64c76161e9bbca60f91
SamuelMedeiroos <150532356+SamuelMedeiroos@users.noreply.github.com>
2023-12-29 20:45:58 -0300

    Add files via upload

---
+++ aula4.rb
@@ -0,0 +1,20 @@
+# 1 - Quero uma saída de dados na tela --> aparecer um comando na tela
+# Quero que o usuário digite meu nome
+print "Digite seu nome:  "
+
+
+
+
+
+# 2 - Programa permita que usuário preencha o que foi pedido
+# gets.chomp
+nome = gets.chomp
+
+print "Digite seu sobrenome:  "
+sobrenome = gets.chomp
+
+print "Digite sua idade:  "
+idade = gets.chomp
+
+# 3 - quero saída final
+puts "Oi #{nome} #{sobrenome}! Sua idade é de: #{idade} "
